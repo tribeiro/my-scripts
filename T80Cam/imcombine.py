4e8e22b1dfdd2dcb57752f90169c9b48623de0c2
tribeiro <tiago.astro@gmail.com>
2015-08-24 16:56:51 -0300

    Improved version of imcombine

--- T80Cam/imcombine.py
+++ T80Cam/imcombine.py
@@ -8,6 +8,7 @@ import sys,os
 import numpy as np
 from astropy.io import fits as pyfits
 import logging
+import datetime as dt
 
 logging.basicConfig(format='%(levelname)s:%(asctime)s::%(message)s',
                     level=logging.DEBUG)
@@ -27,16 +28,21 @@ def main(argv):
 
     nimages = len(args[1:])
     hdu = pyfits.open(args[1])
-    sizex,sizey = hdu[1].data.shape
-    img_stack = np.zeros((nimages,sizex,sizey),dtype=hdu[1].data.dtype)
+    sizex,sizey = hdu[0].data.shape
+    logging.debug('Stack size: %ix%ix%i'%(nimages,sizex,sizey))
+    img_stack = np.zeros((nimages,sizex,sizey),dtype=hdu[0].data.dtype)
+
+    hdu[0].header['COMMENT'] = 'IMCOMBINE: %s'%dt.datetime.now()
+    hdu[0].header['COMMENT'] = 'IMCOMBINE: Combining %i images with median algorithm.'%nimages
 
     for i in range(nimages):
         logging.info('Reading in %s'%args[i+1])
+        hdu[0].header['COMMENT'] = 'IMCOMBINE: %s.'%args[i+1]
         img_stack[i] += pyfits.getdata(args[i+1])
     hdu[0].data = np.median(img_stack,axis=0)
 
     logging.info('Saving output to %s'%opt.output)
-    hdu.writeto(opt.output)
+    hdu.writeto(opt.output,verify='silentfix+warn')
 
     return 0
 
